202505b628de2289c7a499293195208573d0abf9
xdany01 <42984156+xdany01@users.noreply.github.com>
2026-09-05 19:38:25 -0300

    feat(app): Se agrego la plactica con Scanner y EmpleadoForm

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -3,12 +3,14 @@ package org.example.app;
 import org.example.app.practicas.practica01.Practica01;
 import org.example.app.practicas.practica02.Practica02;
 import org.example.app.practicas.practica03.Practica03;
+import org.example.app.practicas.practica04.Practica04;
 
 public class Main {
     static void main() {
 //        testPractica01();
 //        testPractica02();
-        testPractica03();
+//        testPractica03();
+        testPractica04();
     }
 
     static void testPractica01() {
@@ -24,5 +26,11 @@ public class Main {
     static void testPractica03() {
         Practica03 p = new Practica03();
         p.useString();
+    }
+
+    static void testPractica04() {
+        Practica04 p = new Practica04();
+//        p.useScanner();
+        p.useScanner2();
     }
 }
