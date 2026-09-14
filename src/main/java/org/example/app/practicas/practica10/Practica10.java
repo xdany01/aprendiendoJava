8c148f97a139a51f4fb259e30845a48e03d4a87d
xdany01 <42984156+xdany01@users.noreply.github.com>
2026-09-14 18:39:42 -0300

    refactor(app): Renombramiento de variables de filas y columnas

--- src/main/java/org/example/app/practicas/practica10/Practica10.java
+++ src/main/java/org/example/app/practicas/practica10/Practica10.java
@@ -1,22 +1,30 @@
 package org.example.app.practicas.practica10;
 
 public class Practica10 {
-    final char CHAR_PRINTED = '*';
+    private final char charPrinted;
+
+    public Practica10() {
+        charPrinted = '*';
+    }
+
+    public Practica10(char charPrinted) {
+        this.charPrinted = charPrinted;
+    }
 
     public void printTriangle() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
     }
 
     public void printTriangle(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
@@ -24,18 +32,18 @@ public class Practica10 {
 
     public void printTriangleInverse() {
         int rows = 5;
-        for (int i = rows; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = rows; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
     }
 
     public void printTriangleInverse(int rows) {
-        for (int i = rows; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = rows; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
@@ -43,24 +51,24 @@ public class Practica10 {
 
     public void printTriangleEquilateral() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = i; j < rows; j++) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
     }
 
     public void printTriangleEquilateral(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = i; j < rows; j++) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
@@ -68,24 +76,24 @@ public class Practica10 {
 
     public void printTriangleEquilateralInverse() {
         int rows = 5;
-        for (int i = rows; i >= 1; i--) {
-            for (int j = i; j < rows; j++) {
+        for (int row = rows; row >= 1; row--) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
     }
 
     public void printTriangleEquilateralInverse(int rows) {
-        for (int i = rows; i >= 1; i--) {
-            for (int j = i; j < rows; j++) {
+        for (int row = rows; row >= 1; row--) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
@@ -93,30 +101,30 @@ public class Practica10 {
 
     public void printTriangleRight() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = rows - 1; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
     }
 
     public void printTriangleRight(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+        for (int row = rows - 1; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
@@ -124,42 +132,42 @@ public class Practica10 {
 
     public void printTriangleLeft() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = rows; j > i; j--) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = rows; col > row; col--) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = rows; j > i; j--) {
+        for (int row = rows - 1; row >= 1; row--) {
+            for (int col = rows; col > row; col--) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
     }
 
     public void printTriangleLeft(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = rows; j > i; j--) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = rows; col > row; col--) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = rows; j > i; j--) {
+        for (int row = rows - 1; row >= 1; row--) {
+            for (int col = rows; col > row; col--) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
@@ -167,13 +175,13 @@ public class Practica10 {
 
     public void printTriangleEmpty() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = i; j < rows; j++) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1) || i == rows) {
-                    System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                if (col == 1 || col == (2 * row - 1) || row == rows) {
+                    System.out.print(this.charPrinted);
                 } else {
                     System.out.print(" ");
                 }
@@ -183,13 +191,13 @@ public class Practica10 {
     }
 
     public void printTriangleEmpty(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = i; j < rows; j++) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                if (j == 1 || j == (2 * i - 1) || i == rows) {
-                    System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                if (col == 1 || col == (2 * row - 1) || row == rows) {
+                    System.out.print(this.charPrinted);
                 } else {
                     System.out.print(" ");
                 }
@@ -200,10 +208,10 @@ public class Practica10 {
 
     public void printRectangle() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows; j++) {
-                if (i == 1 || i == rows || j == 1 || j == rows) {
-                    System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= rows; col++) {
+                if (row == 1 || row == rows || col == 1 || col == rows) {
+                    System.out.print(this.charPrinted);
                 } else {
                     System.out.print(" ");
                 }
@@ -213,10 +221,10 @@ public class Practica10 {
     }
 
     public void printRectangle(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows; j++) {
-                if (i == 1 || i == rows || j == 1 || j == rows) {
-                    System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= rows; col++) {
+                if (row == 1 || row == rows || col == 1 || col == rows) {
+                    System.out.print(this.charPrinted);
                 } else {
                     System.out.print(" ");
                 }
@@ -227,44 +235,44 @@ public class Practica10 {
 
     public void printDiamond() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = i; j < rows; j++) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
 
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = i; j < rows; j++) {
+        for (int row = rows - 1; row >= 1; row--) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
     }
 
     public void printDiamond(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = i; j < rows; j++) {
+        for (int row = 1; row <= rows; row++) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
 
-        for (int i = rows - 1; i >= 1; i--) {
-            for (int j = i; j < rows; j++) {
+        for (int row = rows - 1; row >= 1; row--) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= (2 * row - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
@@ -272,10 +280,10 @@ public class Practica10 {
 
     public void printCross() {
         int rows = 5;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows; j++) {
-                if (j == i || j == (rows - i + 1)) {
-                    System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= rows; col++) {
+                if (col == row || col == (rows - row + 1)) {
+                    System.out.print(this.charPrinted);
                 } else {
                     System.out.print(" ");
                 }
@@ -285,10 +293,10 @@ public class Practica10 {
     }
 
     public void printCross(int rows) {
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows; j++) {
-                if (j == i || j == (rows - i + 1)) {
-                    System.out.print(CHAR_PRINTED);
+        for (int row = 1; row <= rows; row++) {
+            for (int col = 1; col <= rows; col++) {
+                if (col == row || col == (rows - row + 1)) {
+                    System.out.print(this.charPrinted);
                 } else {
                     System.out.print(" ");
                 }
@@ -299,33 +307,33 @@ public class Practica10 {
 
     public void printHeart() {
         int rows = 6;
-        for (int i = (rows / 2); i <= rows; i += 2) {
+        for (int row = (rows / 2); row <= rows; row += 2) {
 
-            for (int j = 1; j < (rows - i); j += 2) {
+            for (int col = 1; col < (rows - row); col += 2) {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
 
-            for (int j = 1; j <= (rows - i); j++) {
+            for (int col = 1; col <= (rows - row); col++) {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= i; j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= row; col++) {
+                System.out.print(this.charPrinted);
             }
 
             System.out.println();
         }
 
-        for (int i = rows; i >= 1; i--) {
-            for (int j = i; j < rows; j++) {
+        for (int row = rows; row >= 1; row--) {
+            for (int col = row; col < rows; col++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= ((i * 2) - 1); j++) {
-                System.out.print(CHAR_PRINTED);
+            for (int col = 1; col <= ((row * 2) - 1); col++) {
+                System.out.print(this.charPrinted);
             }
             System.out.println();
         }
